54febcf46e853bd1ac7f84bc5512d48b7529e3af
tmptrash@mail.ru <k2rwO(H@>
2014-12-10 22:47:21 +0200

    * Fixed unit tests for Interpreter class

--- jasmine/spec/Interpreter.js
+++ jasmine/spec/Interpreter.js
@@ -10,18 +10,18 @@ describe("Interpreter", function () {
     });
     it('runs simple script', function () {
         int = new Evo.Interpreter();
-        int.run({code: new Uint16Array([0,1,0,0, 14,0,0,0])});
-        expect(int.getOutput()[0]).toBe(1);
+        int.run({code: new Uint16Array([0,1,0,0])});
+        expect(int.getVars()[0]).toBe(1);
     });
     it('tests codeLen config', function () {
         int = new Evo.Interpreter();
-        int.run({code: new Uint16Array([0,1,0,0, 14,0,0,0]), codeLen: 4});
-        expect(int.getOutput().length).toBe(0);
+        int.run({code: new Uint16Array([0,1,0,0, 0,2,0,0]), codeLen: 4});
+        expect(int.getVars()[0]).toBe(1);
     });
     it('tests i config', function () {
         int = new Evo.Interpreter();
-        int.run({code: new Uint16Array([0,1,0,0, 14,0,0,0]), i: 4});
-        expect(int.getOutput()[0]).toBe(0);
+        int.run({code: new Uint16Array([0,1,0,0, 0,2,0,0]), i: 4});
+        expect(int.getVars()[0]).toBe(2);
     });
     it('tests set command', function () {
         int = new Evo.Interpreter();
